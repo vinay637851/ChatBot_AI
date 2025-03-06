98178680295549a8cd1f892670573cab0c05934b
vinaysharma <vs864606@gmail.com>
2025-03-06 23:46:03 +0530

    make floder

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ChatBot_AI
+
+```bash
+conda create -n BloodAIChatbot python=3.10 -y
+
+```bash
+conda activate BloodAIChatbot
+
+```bash
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Blood Bank AI Project',
+    version= '0.0.0',
+    author= 'vinay sharma',
+    author_email= 'vs864606@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
